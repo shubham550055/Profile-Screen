0f47181c0aa45d7f1ddd212681f14762b6ebbf58
shubham550055 <shubhambaviskar5500@gmail.com>
2021-02-28 16:48:46 +0530

    ProfileScreen

--- src/Videofullview.js
+++ src/Videofullview.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import VideoPlayer from 'react-native-video-controls';
+import Video from 'react-native-video';
 
 const Videofullview = () => {
   return (
     <View>
-      <Text>Videofullview Screen</Text>
+      <Video
+        playInBackground={false}
+        playWhenInactive={false}
+        paused={true}
+        disableBack={true}
+        disableFullscreen={true}
+        disableVolume={true}
+        tapAnywhereToPause={true}
+        source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
+        toggleResizeModeOnFullscreen={true}
+        resizeMode={'contain'}
+      />
     </View>
   );
 };
