999f3a92b5f9c92914155f3a4a866e1dc57743eb
shubham550055 <shubhambaviskar5500@gmail.com>
2021-02-28 15:17:59 +0530

    Test

---
+++ src/Videofullview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Videofullview = () => {
+  return (
+    <View>
+      <Text>Videofullview Screen</Text>
+    </View>
+  );
+};
+
+export default Videofullview;
